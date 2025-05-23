fe063e8058e031160057ad05f05d640f52568872
ankit8405 <ankit8405@gmail.com>
2025-05-23 22:44:40 +0530

    Boolean.java

---
+++ Datatypes/Boolean.java
@@ -0,0 +1,21 @@
+// java program to demontrate boolean primitive datatype.
+// class
+
+public class Boolean {
+    // main method
+ public static void main(String []args){
+     // boolean data type is a data type that has one of two possible values
+    // setting boolean to false and  true initially.
+    boolean a=false;
+    boolean b=true;
+    // if condition holds 
+       if(b==true){
+                  // print statement
+                   System.out.println("Hello");
+                  }
+                  else{
+                    System.out.println("bye");
+                  }
+    }
+    
+}
